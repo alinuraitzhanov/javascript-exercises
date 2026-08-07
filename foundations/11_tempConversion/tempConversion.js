325d048b5aa14d7cdc08b7cf8a96e48e1bda8dc3
Alinur Aitzhanov <244883324+alinuraitzhanov@users.noreply.github.com>
2026-08-07 12:02:14 +0500

    Finish 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * (9/5);
+  return Number(Math.round(celsius + 'e' + 1) + 'e-' + 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = (celsius * (9/5) + 32);
+  return Number(Math.round(fahrenheit + 'e' + 1) + 'e-' + 1);
 };
 
 // Do not edit below this line
